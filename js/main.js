9765fd6e656275b7a90f4bca1ed72c076b973957
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-07-27 08:55:52 +1000

    adding getskeleton library

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+  var divEmpty = function () {
+    $("content").hide('slow')
+  }
+
+  $( "#nav-about" ).click(function() {
+     divEmpty()
+     $('#about').animate({height: "toggle"})
+  });
+
+  $( "#nav-projects" ).click(function() {
+    divEmpty()
+    $('#projects').animate({height: "toggle"})
+
+
+  });
+
+  $( "#nav-contact" ).click(function() {
+    divEmpty()
+    $('#contact').animate({height: "toggle"})
+
+  });
+
+  $( "#nav-fun" ).click(function() {
+    // divEmpty()
+    $('#fun').animate({height: "toggle"})
+
+
+  });
+
 });
