9b9242608901571b46253a7ee663246743906b5a
James Theo <jamestheo@Jamess-MacBook-Pro-3.local>
2018-07-14 17:31:40 +1000

    took out some redundant js. slight changes to html and css to accomodate BEM

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,16 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const makisu = require('./makisu.min.js')
-let animated = false
 
 document.addEventListener('DOMContentLoaded', function(){
-  const list = document.getElementsByClassName('list')[0];
-  const navBar = document.getElementById('nav-column');
   const navBarMobile = document.getElementById('nav-column-mobile');
   const mobileNav = document.getElementById('mobile-nav')
-  const header = document.getElementById('header')
   const headingName = document.getElementById('heading-name');
   const content = document.getElementById('content');
   const mobile = document.getElementsByClassName('mobile')
+
   let stuck = false;
   let stickPoint = getDistance(headingName);
 
