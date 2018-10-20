0e9d2808bb9a09ab388f0c9e009abadd670474de
James Theo <jamestheo@Jamess-MacBook-Pro-3.local>
2018-10-20 16:20:26 +1100

    experimenting with css transform and stuff

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,3 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 document.addEventListener('DOMContentLoaded', function(){
 
   const auslanInterpreter = document.getElementById('auslanInterpreteraus');
@@ -153,5 +152,3 @@ document.addEventListener('DOMContentLoaded', function(){
   //     $('.list').makisu('open');
   //   } // ^^^^ this IF statement dictaates how makisu nav animation behaves between mobile and desktop devices ^^^^
   // }
-
-},{}]},{},[1]);
